5e3d1eb3f2e782b518a67067cab39e30c1ea5a76
James <james@Jamesui-MacBookPro.local>
2020-05-07 18:39:14 +0900

    feat: #12550 experimental webpack build issue fix

---
+++ packages/next/compiled/thread-loader/readBuffer.js
@@ -0,0 +1,40 @@
+function readBuffer(pipe, length, callback) {
+  if (length === 0) {
+    callback(null, Buffer.alloc(0));
+    return;
+  }
+
+  let remainingLength = length;
+  const buffers = [];
+
+  const readChunk = () => {
+    const onChunk = (arg) => {
+      let chunk = arg;
+      let overflow;
+      if (chunk.length > remainingLength) {
+        overflow = chunk.slice(remainingLength);
+        chunk = chunk.slice(0, remainingLength);
+        remainingLength = 0;
+      } else {
+        remainingLength -= chunk.length;
+      }
+      buffers.push(chunk);
+      if (remainingLength === 0) {
+        pipe.removeListener('data', onChunk);
+        pipe.pause();
+
+        if (overflow) {
+          pipe.unshift(overflow);
+        }
+
+        callback(null, Buffer.concat(buffers, length));
+      }
+    };
+
+    pipe.on('data', onChunk);
+    pipe.resume();
+  };
+  readChunk();
+}
+
+module.exports = readBuffer;
